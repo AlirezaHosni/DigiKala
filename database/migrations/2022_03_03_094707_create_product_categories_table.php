05ea2d2fec0ffeee36b1665a5b0aecbb21847982
AlirezaHosni <alireza79hosni@gmail.com>
2022-03-03 13:33:14 +0330

    ۲۱۸ و ۲۱۹ کد نداشت ساخت جداول product_categories و brands از ماژول product ۲۲۰

---
+++ database/migrations/2022_03_03_094707_create_product_categories_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('slug')->unique()->nullable();
+            $table->string('image')->nullable();
+            $table->tinyInteger('status')->default(0);
+            $table->tinyInteger('show_in_menu')->default(0);
+            $table->string('tags');
+            $table->foreignId('parent_id')->nullable()->constrained('product_categories')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_categories');
+    }
+}
