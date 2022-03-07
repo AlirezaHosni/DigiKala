9fe73e6eaf16a77382e608d70ac2510a24951eb7
AlirezaHosni <alireza79hosni@gmail.com>
2022-03-07 23:25:28 +0330

    ساخت جداول 228 cart_items, cart_item_selected_values

---
+++ database/migrations/2022_03_07_175832_create_cart_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('product_id')->nullable()->constrained('products')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('color_id')->nullable()->nullable()->constrained('product_colors')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('guarantee_id')->nullable()->nullable()->constrained('guarantees')->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('number')->default(1);
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_items');
+    }
+}
