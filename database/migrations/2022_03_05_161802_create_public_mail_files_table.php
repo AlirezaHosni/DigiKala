6ae2b1030335191533f0c788f4d7877d7ab495d0
AlirezaHosni <alireza79hosni@gmail.com>
2022-03-07 19:55:43 +0330

    ساخت جداول mail,sms,mail files,product_user

---
+++ database/migrations/2022_03_05_161802_create_public_mail_files_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePublicMailFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('public_mail_files', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('public_mail_id')->nullable()->constrained('public_mail')->onDelete('cascade')->onUpdate('cascade');
+            $table->text('file-path');
+            $table->bigInteger('file-size');
+            $table->text('file-type');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('public_mail_files');
+    }
+}
