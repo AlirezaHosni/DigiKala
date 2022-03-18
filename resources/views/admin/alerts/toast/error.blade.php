3312396f9b71d86296954401ba60f9c96c7357b3
AlirezaHosni <alireza79hosni@gmail.com>
2022-03-18 14:32:44 +0330

    add toast alert to ajax 247

--- resources/views/admin/alerts/toast/error.blade.php
+++ resources/views/admin/alerts/toast/error.blade.php
@@ -1,9 +1,9 @@
-@if(session('toast-success'))
+@if(session('toast-error'))
 
     <section class="toast" data-delay="5000">
-        <section class="toast-body py-3 d-flex bg-success text-white">
+        <section class="toast-body py-3 d-flex bg-danger text-white">
             <strong class="ml-auto">
-                {{ session('toast-success') }}
+                {{ session('toast-error') }}
             </strong>
             <button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
--- resources/views/admin/alerts/toast/success.blade.php
+++ resources/views/admin/alerts/toast/success.blade.php
@@ -1,9 +1,9 @@
-@if(session('toast-error'))
+@if(session('toast-success'))
 
     <section class="toast" data-delay="5000">
-        <section class="toast-body py-3 d-flex bg-danger text-white">
+        <section class="toast-body py-3 d-flex bg-success text-white">
             <strong class="ml-auto">
-                {{ session('toast-error') }}
+                {{ session('toast-success') }}
             </strong>
             <button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
