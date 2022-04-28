60ac15687b0412eb9dd227ff5df07a0a45d5ac3c
Alireza Hosni <alireza79hosni@gmail.com>
2022-04-29 02:35:15 +0430

    image service(5) add createAndSave to ImageService.php 254

--- app/Http/Services/Image/ImageService.php
+++ app/Http/Services/Image/ImageService.php
@@ -5,6 +5,7 @@ namespace App\Http\Services\Image;
 
 
 use Intervention\Image\Facades\Image;
+use League\Flysystem\Config;
 
 class ImageService extends ImageToolsService
 {
@@ -37,5 +38,47 @@ class ImageService extends ImageToolsService
         // $_FILES['image']['tmp_name']  old php
 
         return $result ? $this->getImageAddress() : false ;
+    }
+
+    public function createAndSave($image): bool
+    {
+        // get data from config
+        $imageSizes = Config::get('image.index-image-sizes');
+
+        //set image
+        $this->setImage($image);
+
+        // set directory
+        $this->getImageDirectory() ?? $this->setImageDirectory(date('Y') . DIRECTORY_SEPARATOR . date('m') . DIRECTORY_SEPARATOR . date('d'));
+        $this->setImageDirectory($this->getImageDirectory() . DIRECTORY_SEPARATOR . time());
+
+        // set name
+        $this->getImageName() ?? $this->setImageName(time());
+        $imageName = $this->getImageName();
+
+        $indexArray = [];
+        foreach ($imageSizes as $sizeAliases => $imageSize)
+        {
+            // create and set this size name
+            $currentImageName = $imageName . '-' . $sizeAliases;
+            $this->setImageName($currentImageName);
+
+            // execute image
+            $this->provider();
+
+            // save image
+            $result = Image::make($image->getRealPath())->fit($imageSize['width'], $imageSize['height'])->save(public_path($this->getImageAddress()), null, $this->getImageFormat()); // laravel and modern php
+
+            if ($result)
+                $indexArray[$sizeAliases] = $this->getImageAddress();
+            else
+                return false;
+        }
+
+        $image['indexArray'] = $indexArray;
+        $image['directory'] = $this->getFinalImageDirectory();
+        $image['currentImage'] = Config::get('image.default-current-index-image');
+
+        return $image;
     }
 }
