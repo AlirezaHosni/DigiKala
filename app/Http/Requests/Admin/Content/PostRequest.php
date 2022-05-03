a25ea0d680d0b3ed05b9b313cf6ad8f59433a3ef
Alireza Hosni <alireza79hosni@gmail.com>
2022-05-03 12:52:53 +0430

    validation , fix duplicate slug error 263

--- app/Http/Requests/Admin/Content/PostRequest.php
+++ app/Http/Requests/Admin/Content/PostRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Admin\Content;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class PostCategoryRequest extends FormRequest
+class PostRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -25,32 +25,27 @@ class PostCategoryRequest extends FormRequest
     {
         if($this->isMethod('post')){
             return [
-                'name' => 'required|max:120|min:2|regex:/^[a-zA-Z\-۰-۹0-9ء-ي. ا-ی]+$/u',
-                'description' => 'required|max:500|min:5|regex:/^[a-zA-Z\-۰-۹0-9ء-ي.ا-ی><\/&;/n/r ]+$/u',
+                'title' => 'required|max:120|min:2|regex:/^[a-zA-Z\-۰-۹0-9ء-ي. ا-ی]+$/u',
+                'summary' => 'required|max:300|min:5|regex:/^[a-zA-Z\-۰-۹0-9ء-ي.ا-ی><\/&;/n/r ]+$/u',
+                'body' => 'required|max:600|min:5|regex:/^[a-zA-Z\-۰-۹0-9ء-ي.ا-ی><\/&;/n/r ]+$/u',
+                'category_id' => 'required|min:1|regex:/^[0-9]+$/u|exist:post_categories,id',
                 'image' => 'required|image|mimes:png,jpg,jpeg,gif',
                 'status' => 'required|numeric|in:0,1',
                 'tags' => 'required|regex:/^[a-zA-Z\-۰-۹0-9ء-ي. ا-ی]+$/u',
+                'published_at' => 'required|numeric'
             ];
         }
         else {
             return [
-                'name' => 'required|max:120|min:2|regex:/^[a-zA-Z\-۰-۹0-9ء-ي. ا-ی]+$/u',
-                'description' => 'required|max:500|min:5|regex:/^[a-zA-Z\-۰-۹0-9ء-ي.ا-ی><\/&;\n\r ]+$/u',
+                'title' => 'required|max:120|min:2|regex:/^[a-zA-Z\-۰-۹0-9ء-ي. ا-ی]+$/u',
+                'summary' => 'required|max:300|min:5|regex:/^[a-zA-Z\-۰-۹0-9ء-ي.ا-ی><\/&;/n/r ]+$/u',
+                'body' => 'required|max:600|min:5|regex:/^[a-zA-Z\-۰-۹0-9ء-ي.ا-ی><\/&;/n/r ]+$/u',
+                'category_id' => 'required|min:1|regex:/^[0-9]+$/u|exist:post_categories,id',
                 'image' => 'image|mimes:png,jpg,jpeg,gif',
                 'status' => 'required|numeric|in:0,1',
                 'tags' => 'required|regex:/^[a-zA-Z\-۰-۹0-9ء-ي. ا-ی]+$/u',
+                'published_at' => 'numeric'
             ];
         }
     }
-
-//    public function attributes()
-//    {
-//        return [
-//            'name' => 'نام دسته‌بندی',
-//            'description' => 'توضیحات',
-//            'image' => 'تصویر',
-//            'status' => 'وضعیت',
-//            'tags' => 'تگ‌ها',
-//        ];
-//    }
 }
