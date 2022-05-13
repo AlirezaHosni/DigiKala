7540bdd0c4f564515fcd76a5c26904a7812f3081
Alireza Hosni <alireza79hosni@gmail.com>
2022-05-13 14:11:33 +0430

    comment section(2) answer, approved, relations 274

--- app/Http/Controllers/Admin/Content/CommentController.php
+++ app/Http/Controllers/Admin/Content/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin\Content;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Content\CommentRequest;
 use App\Models\Content\Comment;
 use App\Models\Content\Post;
 use Illuminate\Http\Request;
@@ -16,7 +17,12 @@ class CommentController extends Controller
      */
     public function index()
     {
-        $comments = Comment::where('commentable_type', Post::class)->orderBy('created_at', 'desc')->simplePaginate(15);
+        $unSeenComments = Comment::where('seen', 0)->get();
+        foreach ($unSeenComments as $unSeenComment){
+            $unSeenComment->seen = 1;
+            $result = $unSeenComment->save();
+        }
+        $comments = Comment::where('commentable_type', Post::class)->orderBy('created_at', 'desc')->with('commentable')->simplePaginate(15);
         return view('admin.content.comment.index', compact('comments'));
     }
 
@@ -33,7 +39,7 @@ class CommentController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -44,7 +50,7 @@ class CommentController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
      */
     public function show(Comment $comment)
@@ -55,7 +61,7 @@ class CommentController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -66,8 +72,8 @@ class CommentController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -78,7 +84,7 @@ class CommentController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
@@ -92,21 +98,51 @@ class CommentController extends Controller
         $comment->status = $comment->status == 0 ? 1 : 0;
         $result = $comment->save();
 
-        if($result){
-            if($comment->status == 0){
+        if ($result) {
+            if ($comment->status == 0) {
                 return response()->json([
                     'status' => true,
                     'checked' => false
                 ]);
-            }else{
+            } else {
                 return response()->json([
                     'status' => true,
                     'checked' => true
                 ]);
             }
 
+        } else {
+            return response()->json(['status' => false]);
+        }
+    }
+
+    public function approved(Comment $comment)
+    {
+
+        $comment->approved = $comment->approved == 0 ? 1 : 0;
+        $result = $comment->save();
+
+        if ($result) {
+            return redirect()->route('admin.content.comment.index')->with('swal-success', 'تغییر وضعیت نظر با موفقیت انجام شد');
+        } else {
+            return redirect()->route('admin.content.comment.index')->with('swal-error', 'تغییر وضعیت نظر با خطا مواجه شد');
+        }
+    }
+
+    public function answer(CommentRequest $request, Comment $comment)
+    {
+        if ($comment->parent_id == null) {
+            $inputs = $request->all();
+            $inputs['author_id'] = 1;
+            $inputs['parent_id'] = $comment->id;
+            $inputs['commentable_id'] = $comment->commentable_id;
+            $inputs['commentable_type'] = $comment->commentable_type;
+            $inputs['approved'] = 1;
+            $inputs['status'] = 1;
+            $comment = Comment::create($inputs);
+            return redirect()->route('admin.content.comment.index')->with('swal-success', 'پاسخ شما با موفقیت ثبت شد');
         }else{
-            return response()->json(['status' => false ]);
+            return redirect()->route('admin.content.comment.index')->with('swal-error', 'خطا');
         }
     }
 }
