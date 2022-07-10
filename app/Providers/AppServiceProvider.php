163643484ccb33f7d77df5df211db5dbe2290d9c
Alireza Hosni <alireza79hosni@gmail.com>
2022-07-10 12:57:48 +0430

    unseen comments 319

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Content\Comment;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        view()->composer('admin.layouts.header', function ($view){
+            $view->with('unseenComments', Comment::where('seen', 0)->get());
+        });
     }
 }
