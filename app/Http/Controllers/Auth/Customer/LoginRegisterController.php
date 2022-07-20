792b9c743a27b78a3c4319f2a1b98b31fc6ae483
Alireza Hosni <alireza79hosni@gmail.com>
2022-07-20 18:16:10 +0430

    send email 329

--- app/Http/Controllers/Auth/Customer/LoginRegisterController.php
+++ app/Http/Controllers/Auth/Customer/LoginRegisterController.php
@@ -10,6 +10,7 @@ use App\Http\Services\Message\SMS\SmsService;
 use App\Models\Otp;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Str;
 
 class LoginRegisterController extends Controller
@@ -24,7 +25,7 @@ class LoginRegisterController extends Controller
         $inputs = $request->all();
 
         //check id is email or not
-        if (filter_var($inputs['id'], FILTER_VALIDATE_EMAIL))
+        if(filter_var($inputs['id'], FILTER_VALIDATE_EMAIL))
         {
             $type = 1; // 1 => email
             $user = User::where('email', $inputs['id'])->first();
@@ -32,69 +33,76 @@ class LoginRegisterController extends Controller
                 $newUser['email'] = $inputs['id'];
             }
         }
+
         //check id is mobile or not
-        elseif (preg_match('/^[\+98|098|98|0]9\d{9}$/', $inputs['id']))
-        {
-            $type = 0; // 0 => email
-            // all mobile numbers are in one format 9** *** ****
+        elseif(preg_match('/^(\+98|98|0)9\d{9}$/', $inputs['id'])){
+            $type = 0; // 0 => mobile;
+
+
+            // all mobile numbers are in on format 9** *** ***
             $inputs['id'] = ltrim($inputs['id'], '0');
-            $inputs['id'] = substr($inputs['id'], 0, 2) === '98' ? substr($inputs['id'],2) : $inputs['id'];
+            $inputs['id'] = substr($inputs['id'], 0, 2) === '98' ? substr($inputs['id'], 2) : $inputs['id'];
             $inputs['id'] = str_replace('+98', '', $inputs['id']);
 
             $user = User::where('mobile', $inputs['id'])->first();
             if(empty($user)){
                 $newUser['mobile'] = $inputs['id'];
             }
-        }else{
-            $errorText = 'شناسه وارد شده نه شماره موبایل است نه ایمیل';
-            return redirect()->route('auth.customer.login-register-fo   rm')->withErrors(['id' => $errorText]);
         }
 
-        if (empty($user)){
+        else{
+            $errorText = 'شناسه ورودی شما نه شماره موبایل است نه ایمیل';
+            return redirect()->route('auth.customer.login-register-form')->withErrors(['id' => $errorText]);
+        }
+
+        if(empty($user)){
             $newUser['password'] = '98355154';
             $newUser['activation'] = 1;
             $user = User::create($newUser);
         }
 
-        // create otp code
+        //create otp code
         $otpCode = rand(111111, 999999);
         $token = Str::random(60);
         $otpInputs = [
-            'otp_code' => $otpCode,
             'token' => $token,
             'user_id' => $user->id,
+            'otp_code' => $otpCode,
+            'login_id' => $inputs['id'],
             'type' => $type,
-            'login_id' => $inputs['id']
         ];
 
         Otp::create($otpInputs);
 
-        // send sms or email
-        if ($type == 0){
-            // sms
+        //send sms or email
+
+        if($type == 0){
+            //send sms
             $smsService = new SmsService();
-            $smsService->setFrom(config('sms.otp_from'));
+            $smsService->setFrom(Config::get('sms.otp_from'));
             $smsService->setTo(['0' . $user->mobile]);
-            $smsService->setText("مجموعه آمازون\n کد تایید:$otpCode");
+            $smsService->setText("مجموعه آمازون \n  کد تایید : $otpCode");
             $smsService->setIsFlash(true);
 
-            $messageService = new MessageService($smsService);
-        }else{
+            $messagesService = new MessageService($smsService);
+
+        }
+
+        elseif($type === 1){
             $emailService = new EmailService();
             $details = [
-                'title' => 'ایمیل فعال‌سازی',
-                'body' => "کد فعال‌سازی شما : $otpCode"
+                'title' => 'ایمیل فعال سازی',
+                'body' => "کد فعال سازی شما : $otpCode"
             ];
             $emailService->setDetails($details);
             $emailService->setFrom('noreply@example.com', 'example');
             $emailService->setSubject('کد احراز هویت');
             $emailService->setTo($inputs['id']);
 
-            $messageService = new MessageService($emailService);
-//            dd($emailService, $messageService);
+            $messagesService = new MessageService($emailService);
 
         }
 
-        dd($messageService->send());
+        $messagesService->send();
     }
 }
